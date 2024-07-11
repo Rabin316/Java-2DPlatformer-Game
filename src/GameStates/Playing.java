6b77d576b33b2ff9c5942cdfc754b19145c451f9
Rabin316 <rabindangol12345@gmail.com>
2024-07-11 16:50:23 +0545

    Added VolumeButton

--- src/GameStates/Playing.java
+++ src/GameStates/Playing.java
@@ -13,7 +13,7 @@ public class Playing extends State implements Statemethods {
     private Player player;
     private LevelManager levelManager;
     private PauseOverlay pauseOverlay;
-    private boolean paused = true;
+    private boolean paused = false;
 
     public Playing(Game game) {
         super(game);
@@ -24,7 +24,7 @@ public class Playing extends State implements Statemethods {
         player = new Player(200, 200, (int) (64 * Game.SCALE), (int) (40 * Game.SCALE));
         levelManager = new LevelManager(game);
         player.loadLvlData(levelManager.getCurrentLevel().getLevelData());
-        pauseOverlay = new PauseOverlay();
+        pauseOverlay = new PauseOverlay(this);
     }
 
     public void windowFocusLost() {
@@ -37,16 +37,20 @@ public class Playing extends State implements Statemethods {
 
     @Override
     public void update() {
-        levelManager.update();
-        player.update();
-        pauseOverlay.update();
+        if (!paused) {
+            levelManager.update();
+            player.update();
+        } else {
+            pauseOverlay.update();
+        }
     }
 
     @Override
     public void draw(Graphics g) {
         levelManager.draw(g);
         player.render(g);
-        pauseOverlay.draw(g);
+        if (paused)
+            pauseOverlay.draw(g);
     }
 
     @Override
@@ -74,6 +78,10 @@ public class Playing extends State implements Statemethods {
         ;
     }
 
+    public void unpauseGame() {
+        paused = false;
+    }
+
     @Override
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()) {
@@ -88,8 +96,9 @@ public class Playing extends State implements Statemethods {
             case KeyEvent.VK_SPACE:
                 player.setJump(true);
                 break;
-            case KeyEvent.VK_BACK_SPACE:
-                Gamestate.state = Gamestate.MENU;
+            case KeyEvent.VK_ESCAPE:
+                paused=!paused;
+                break;
         }
     }
 
